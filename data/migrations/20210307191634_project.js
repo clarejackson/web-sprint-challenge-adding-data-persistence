e84afbc786ff2cc46268117326eb2ade76ad2a3e
Clare Jackson <clare-jackson@lambdastudents.com>
2021-03-07 14:26:25 -0500

    first migration

--- data/migrations/20210307191634_project.js
+++ data/migrations/20210307191634_project.js
@@ -1,8 +1,14 @@
+const { Knex } = require("knex");
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await Knex.schema.createTable('projects', (table) => {
+    table.increments("project_id")
+    table.text("project_name").notNull()
+    table.text("project_description")
+    table.integer("project_completed").defaultTo(0)
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.dropTableIfExists('projects')
 };

02d4d8561e5077be202d6718a324e6fef421c2c9
Clare Jackson <clare-jackson@lambdastudents.com>
2021-03-07 21:04:21 -0500

    cleaned it up

--- api/project/router.js
+++ api/project/router.js
@@ -14,11 +14,6 @@ router.get('/', async (req, res, next) => {
 })
 
 
-// [POST] /api/projects
-
-// Even though project_completed is stored as an integer, the API uses booleans when interacting with the client
-// Example of response body: {"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}
-// not working
 router.post('/', async (req, res, next) => {
   try {
     const projects = await model.addProject(req.body)
